ae2be6074a66eb6d8c1436b0950f5aa4121a5e42
czw <chenzhengwei@espressif.com>
2020-05-28 17:58:12 +0800

    feature(lcd):Optimization of the LCD

--- esp32-s2-kaluga-1/examples/lcd/main/main.c
+++ esp32-s2-kaluga-1/examples/lcd/main/main.c
@@ -1,84 +1,160 @@
-/* LCD example
+/* Camera Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+    This example code is in the Public Domain (or CC0 licensed, at your option.)
+    Unless required by applicable law or agreed to in writing, this
+    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+    CONDITIONS OF ANY KIND, either express or implied.
 */
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
-
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-#include "driver/spi_master.h"
-#include "driver/gpio.h"
-
-#include "esp_system.h"
+#include "esp_heap_caps.h"
 #include "esp_log.h"
-#include "esp_err.h"
-#include "pretty_effect.h"
+#include "esp_spiffs.h"
 #include "lcd.h"
-
-/*
- This code displays some fancy graphics on the 320x240 LCD on an ESP32-S2-KALUGA board.
- This example demonstrates the use of both spi_device_transmit as well as
- spi_device_queue_trans/spi_device_get_trans_result and pre-transmit callbacks.
-
- Some info about the ILI9341/ST7789V: It has an C/D line, which is connected to a GPIO here. It expects this
- line to be low for a command and high for data. We use a pre-transmit callback here to control that
- line: every transaction has as the user-definable argument the needed state of the D/C line and just
- before the transaction is sent, the callback will set this line to the correct state.
-*/
-
-/*!< Reference the binary-included jpeg fil */
-/*!< image.jpg == _binary_image_jpg_start */
-extern const uint8_t image_jpg_start[] asm("_binary_image_jpg_start");
-extern const uint8_t image_jpg_end[] asm("_binary_image_jpg_end");
-/*!< Define the height and width of the jpeg file. Make sure this matches the actual jpeg dimensions*/
-
-#define IMAGE_W 336 /*!< width of jpeg file */
-#define IMAGE_H 256 /*!< height of jpeg file */
+#include "jpeg.h"
+#include "board.h"
 
 static const char *TAG = "main";
 
-void app_main(void)
+#define IMAGE_MAX_SIZE (100 * 1024)/**< The maximum size of a single picture in the boot animation */
+#define IMAGE_WIDTH    320 /*!< width of jpeg file */
+#define IMAGE_HIGHT    240 /*!< height of jpeg file */
+
+/**
+ * @brief rgb -> rgb565
+ *
+ * @param r red   (0~31)
+ * @param r green (0~31)
+ * @param r red   (0~31)
+ *
+ * @return data about color565
+ */
+uint16_t color565(uint8_t r, uint8_t g, uint8_t b)
 {
-    spi_device_handle_t spi = {0};
-    spi_bus_config_t bus_cfg = {
-        .miso_io_num     = PIN_NUM_MISO,
-        .mosi_io_num     = PIN_NUM_MOSI,
-        .sclk_io_num     = PIN_NUM_CLK,
-        .quadwp_io_num   = -1,
-        .quadhd_io_num   = -1,
-        .max_transfer_sz = PARALLEL_LINES * 320 * 2 + 8
-    };
-    spi_device_interface_config_t devcfg = {
-#ifdef CONFIG_LCD_OVERCLOCK
-        .clock_speed_hz = 26 * 1000 * 1000,              /*!< Clock out at 26 MHz */
-#else
-        .clock_speed_hz = 10 * 1000 * 1000,              /*!< Clock out at 10 MHz */
-#endif
-        .mode           = 0,                             /*!< SPI mode 0 */
-        .spics_io_num   = PIN_NUM_CS,                    /*!< CS pin */
-        .queue_size     = 7,                             /*!< We want to be able to queue 7 transactions at a time */
-        .pre_cb         = lcd_spi_pre_transfer_callback, /*!< Specify pre-transfer callback to handle D/C line */
-    };
-    /*!< Initialize the SPI bus */
-    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &bus_cfg, DMA_CHAN));
-    /*!< Attach the LCD to the SPI bus */
-    ESP_ERROR_CHECK(spi_bus_add_device(LCD_HOST, &devcfg, &spi));
-    /**< Initialize the LCD */
-    ESP_ERROR_CHECK(lcd_init(spi));
-
-    /*!< Initialize and Decode the image , cache it in memory */
-    ESP_LOGI(TAG, "the memory get: %d", esp_get_free_heap_size());
-    ESP_ERROR_CHECK(pretty_effect_init(image_jpg_start, IMAGE_W, IMAGE_H));
-    ESP_LOGI(TAG ,"the memory get: %d", esp_get_free_heap_size());
-
-    /*!< Displays static or dynamic image. */
-    display_pretty_colors(spi);
+    uint16_t color = ((r  << 11) | (g  << 6) | b);
+    return (color << 8) | (color >> 8);
 }
+
+void esp_photo_display(void)
+{
+    ESP_LOGI(TAG, "LCD photo test....");
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false
+    };
+
+    /*!< Use settings defined above to initialize and mount SPIFFS filesystem. */
+    /*!< Note: esp_vfs_spiffs_register is an all-in-one convenience function. */
+    ESP_ERROR_CHECK(esp_vfs_spiffs_register(&conf));
+    size_t total = 0, used = 0;
+    ESP_ERROR_CHECK(esp_spiffs_info(NULL, &total, &used));
+
+    uint8_t *img = NULL;
+    uint8_t *buf = malloc(IMAGE_MAX_SIZE);
+    int read_bytes = 0;
+    int width = 0, height = 0;
+
+    FILE *fd = fopen("/spiffs/image.jpg", "r");
+
+    read_bytes = fread(buf, 1, IMAGE_MAX_SIZE, fd);
+    ESP_LOGI(TAG, "spiffs:read_bytes:%d  fd: %p", read_bytes, fd);
+    fclose(fd);
+
+    img = jpeg_decode(buf, &width, &height);
+    lcd_set_index(0, 0, IMAGE_WIDTH - 1, IMAGE_HIGHT - 1);
+    lcd_write_data(img, IMAGE_WIDTH * IMAGE_HIGHT * sizeof(uint16_t));
+    free(buf);
+    vTaskDelay(2000 / portTICK_RATE_MS);
+}
+
+void esp_color_display(void)
+{
+    ESP_LOGI(TAG, "LCD color test....");
+    uint16_t *data_buf = (uint16_t *)heap_caps_calloc(IMAGE_WIDTH * IMAGE_HIGHT, sizeof(uint16_t), MALLOC_CAP_SPIRAM);
+
+    while (1) {
+        uint16_t color = color565(0, 0, 0);
+
+        for (int r = 0,  j = 0; j < IMAGE_HIGHT; j++) {
+            if (j % 8 == 0) {
+                color = color565(r++, 0, 0);
+            }
+
+            for (int i = 0; i < IMAGE_WIDTH; i++) {
+                data_buf[i + IMAGE_WIDTH * j] = color;
+            }
+        }
+
+        lcd_set_index(0, 0, IMAGE_WIDTH - 1, IMAGE_HIGHT - 1);
+        lcd_write_data((uint8_t *)data_buf, IMAGE_WIDTH * IMAGE_HIGHT * sizeof(uint16_t));
+        vTaskDelay(2000 / portTICK_RATE_MS);
+
+        for (int g = 0,  j = 0; j < IMAGE_HIGHT; j++) {
+            if (j % 8 == 0) {
+                color = color565(0, g++, 0);
+            }
+
+            for (int i = 0; i < IMAGE_WIDTH; i++) {
+                data_buf[i + IMAGE_WIDTH * j] = color;
+            }
+        }
+
+        lcd_set_index(0, 0, IMAGE_WIDTH - 1, IMAGE_HIGHT - 1);
+        lcd_write_data((uint8_t *)data_buf, IMAGE_WIDTH * IMAGE_HIGHT * sizeof(uint16_t));
+        vTaskDelay(2000 / portTICK_RATE_MS);
+
+        for (int b = 0,  j = 0; j < IMAGE_HIGHT; j++) {
+            if (j % 8 == 0) {
+                color = color565(0, 0, b++);
+            }
+
+            for (int i = 0; i < IMAGE_WIDTH; i++) {
+                data_buf[i + IMAGE_WIDTH * j] = color;
+            }
+        }
+
+        lcd_set_index(0, 0, IMAGE_WIDTH - 1, IMAGE_HIGHT - 1);
+        lcd_write_data((uint8_t *)data_buf, IMAGE_WIDTH * IMAGE_HIGHT * sizeof(uint16_t));
+        vTaskDelay(2000 / portTICK_RATE_MS);
+
+    }
+}
+
+void app_main()
+{
+    lcd_config_t lcd_config = {
+#ifdef CONFIG_LCD_ST7789
+        .clk_fre         = 80 * 1000 * 1000, /*!< ILI9341 Stable frequency configuration */
+#endif
+#ifdef CONFIG_LCD_ILI9341
+        .clk_fre         = 40 * 1000 * 1000, /*!< ILI9341 Stable frequency configuration */
+#endif
+        .pin_clk         = LCD_CLK,
+        .pin_mosi        = LCD_MOSI,
+        .pin_dc          = LCD_DC,
+        .pin_cs          = LCD_CS,
+        .pin_rst         = LCD_RST,
+        .pin_bk          = LCD_BK,
+        .max_buffer_size = 2 * 1024,
+        .horizontal      = 2 /*!< 2: UP, 3: DOWN */
+    };
+
+    lcd_init(&lcd_config);
+
+    /*< Show a picture */
+    esp_photo_display();
+    /*< RGB display */
+    esp_color_display();
+
+}
+
