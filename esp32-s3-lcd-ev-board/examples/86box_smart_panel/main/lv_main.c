29645bf01250e8e9584f631a24142520447d2be9
Liu Zhongwei <liuzhongwei@espressif.com>
2023-08-21 12:05:46 +0800

    fix missing IO control for ESP32_S3_LCD_EV_BOARD

--- esp32-s3-lcd-ev-board/examples/86box_smart_panel/main/lv_main.c
+++ esp32-s3-lcd-ev-board/examples/86box_smart_panel/main/lv_main.c
@@ -51,12 +51,16 @@ void app_main(void)
     ESP_ERROR_CHECK(err);
     ESP_ERROR_CHECK(settings_read_parameter_from_nvs());
 
+#if CONFIG_BSP_LCD_SUB_BOARD_480_480
+    // For the newest version sub board, we need to set `BSP_LCD_VSYNC` to high before initialize LCD
+    // It's a requirement of the LCD module and will be added into BSP in the future
     gpio_config_t io_conf = {};
     io_conf.pin_bit_mask = BIT64(BSP_LCD_VSYNC);
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pull_up_en = true;
     gpio_config(&io_conf);
     gpio_set_level(BSP_LCD_VSYNC, 1);
+#endif
 
     bsp_i2c_init();
     bsp_display_start();
--- esp32-s3-lcd-ev-board/examples/usb_camera_lcd/main/main.c
+++ esp32-s3-lcd-ev-board/examples/usb_camera_lcd/main/main.c
@@ -61,6 +61,17 @@ static void camera_frame_cb(uvc_frame_t *frame, void *ptr);
 
 void app_main(void)
 {
+#if CONFIG_BSP_LCD_SUB_BOARD_480_480
+    // For the newest version sub board, we need to set `BSP_LCD_VSYNC` to high before initialize LCD
+    // It's a requirement of the LCD module and will be added into BSP in the future
+    gpio_config_t io_conf = {};
+    io_conf.pin_bit_mask = BIT64(BSP_LCD_VSYNC);
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pull_up_en = true;
+    gpio_config(&io_conf);
+    gpio_set_level(BSP_LCD_VSYNC, 1);
+#endif
+
     void *lcd_usr_data = NULL;
 #if CONFIG_BSP_LCD_SUB_BOARD_480_480
     ESP_ERROR_CHECK(bsp_i2c_init());
